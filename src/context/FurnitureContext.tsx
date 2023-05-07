6ad383c38ea660ad3f189c420231081290500a94
David Forero <54685136+David-forero@users.noreply.github.com>
2023-05-07 11:07:55 -0400

    consumiendo apis de muebles

--- src/context/FurnitureContext.tsx
+++ src/context/FurnitureContext.tsx
@@ -1,24 +1,54 @@
-import { useContext, createContext, ReactNode } from "react";
+import {
+  useContext,
+  createContext,
+  ReactNode,
+  useState,
+  useCallback,
+  useEffect,
+} from "react";
+import { get } from "../common/functions/http";
 
 interface IFurnitureContext {
-  // variables
+  furnitures: any;
+  getFurniture: any;
+  furniture: any;
 }
 
 interface IFornitureProviderProps {
   children: ReactNode;
 }
 
-const FurnitureContext = createContext<IFurnitureContext>({} as IFurnitureContext);
+const FurnitureContext = createContext<IFurnitureContext>(
+  {} as IFurnitureContext
+);
 
 const FurnitureProvider = ({ children }: IFornitureProviderProps) => {
+  const [furnitures, setFurnitures] = useState(null);
+  const [furniture, setFurniture] = useState(null);
+
+  const getFurnitures = useCallback(async () => {
+    const { data } = await get("/furnitures");
+    setFurnitures(data.data);
+  }, []);
+
+  const getFurniture = useCallback(async (id: string) => {
+    const { data } = await get(`/furnitures/${id}`);
+    setFurniture(data.data);
+  }, []);
+
+  useEffect(() => {
+    getFurnitures();
+  }, []);
+
   return (
     <FurnitureContext.Provider
-      value={
-        {
-          /*🔻  Variables 🔻*/
-          /*🔻  Funciones 🔻*/
-        }
-      }
+      value={{
+        /*🔻  Variables 🔻*/
+        furnitures,
+        furniture,
+        /*🔻  Funciones 🔻*/
+        getFurniture,
+      }}
     >
       {children}
     </FurnitureContext.Provider>
