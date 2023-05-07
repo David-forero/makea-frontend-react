699b78ab5f0dac548c65226f80da32d8a1f3bd7e
David Forero <54685136+David-forero@users.noreply.github.com>
2023-05-07 09:04:40 -0400

    correciones de error logs

--- src/common/components/CardProduct.tsx
+++ src/common/components/CardProduct.tsx
@@ -11,6 +11,8 @@ type Props = {
 
 const CardProduct = ({}: Props) => {
   const navigate = useNavigate();
+  console.log(import.meta.env.VITE_SOME_KEY);
+  
 
   return (
     <div className="group relative" onClick={() => navigate('/furniture/34124234234')}>
