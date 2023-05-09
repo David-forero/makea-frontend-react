70f2c52d450d3256532dfae95214de84578010cc
David Forero <54685136+David-forero@users.noreply.github.com>
2023-05-09 12:59:18 -0400

    cargando loading

--- src/pages/DetailsFurniture.tsx
+++ src/pages/DetailsFurniture.tsx
@@ -1,99 +1,172 @@
-import { useEffect } from "react"
-import { useFurnitureContext } from "../context/FurnitureContext"
+import { useEffect } from "react";
+import { useFurnitureContext } from "../context/FurnitureContext";
 import { useParams } from "react-router-dom";
 import { useBasketContext } from "../context/BasketContext";
 import { useAuthContext } from "../context/AuthContext";
 
 const DetailsFurniture = () => {
-  const {addToBasket} = useBasketContext()
-  const {auth} = useAuthContext();
-  const {getFurniture, furniture} = useFurnitureContext();
-  const {furnitureId} = useParams();
+  const [loading, setLoading] = useState(true);
+  const { addToBasket } = useBasketContext();
+  const { auth } = useAuthContext();
+  const { getFurniture, furniture } = useFurnitureContext();
+  const { furnitureId } = useParams();
 
   async function init() {
-    await getFurniture(furnitureId)
+    await getFurniture(furnitureId, setLoading);
   }
 
   useEffect(() => {
+    setLoading(true);
     init();
-  }, [])
-  
+  }, []);
 
   return (
     <div className="bg-white">
-    <div className="pt-6">
-     
-  
-      <div className="mx-auto max-w-lg">
-        <div className="aspect-w-3">
-          <img src={furniture?.imgUrl} alt="Two each of gray, white, and black shirts laying flat." className="h-2/5 w-full object-cover object-center"/>
-        </div>
-        
-      </div>
-  
-      {/* <!-- Product info --> */}
-      <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 ">
-        <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-          <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{furniture?.title}</h1>
-        </div>
-  
-        {/* <!-- Options --> */}
-        <div className="mt-4 lg:row-span-3 lg:mt-0">
-          <h2 className="sr-only">Información del producto</h2>
-          <p className="text-3xl tracking-tight text-green-600">${furniture?.price}</p>
-  
-          {/* <!-- Reviews --> */}
-          <div className="mt-6">
-            <h3 className="sr-only">Reviews</h3>
-            <div className="flex items-center">
-              <div className="flex items-center">
-                {/* <!-- Active: "text-gray-900", Default: "text-gray-200" --> */}
-                <svg className="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
-                </svg>
-                <svg className="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
-                </svg>
-                <svg className="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
-                </svg>
-                <svg className="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
-                </svg>
-                <svg className="text-gray-200 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
-                </svg>
+      {loading ? (
+        <h1 className="text-dark text-text font-bold text-4xl">
+          Cargando producto...
+        </h1>
+      ) : (
+        <div className="pt-6">
+          <div className="mx-auto max-w-lg">
+            <div className="aspect-w-3">
+              <img
+                src={furniture?.imgUrl}
+                alt="Two each of gray, white, and black shirts laying flat."
+                className="h-2/5 w-full object-cover object-center"
+              />
+            </div>
+          </div>
+
+          {/* <!-- Product info --> */}
+          <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 ">
+            <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+              <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                {furniture?.title}
+              </h1>
+            </div>
+
+            {/* <!-- Options --> */}
+            <div className="mt-4 lg:row-span-3 lg:mt-0">
+              <h2 className="sr-only">Información del producto</h2>
+              <p className="text-3xl tracking-tight text-green-600">
+                ${furniture?.price}
+              </p>
+
+              {/* <!-- Reviews --> */}
+              <div className="mt-6">
+                <h3 className="sr-only">Reviews</h3>
+                <div className="flex items-center">
+                  <div className="flex items-center">
+                    {/* <!-- Active: "text-gray-900", Default: "text-gray-200" --> */}
+                    <svg
+                      className="text-gray-900 h-5 w-5 flex-shrink-0"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <svg
+                      className="text-gray-900 h-5 w-5 flex-shrink-0"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <svg
+                      className="text-gray-900 h-5 w-5 flex-shrink-0"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <svg
+                      className="text-gray-900 h-5 w-5 flex-shrink-0"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <svg
+                      className="text-gray-200 h-5 w-5 flex-shrink-0"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                  <p className="sr-only">4 de 5 estrellas</p>
+                  <a
+                    href="#"
+                    className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    117 reviews
+                  </a>
+                </div>
               </div>
-              <p className="sr-only">4 de 5 estrellas</p>
-              <a href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">117 reviews</a>
-            </div>
-          </div>
-  
-          <form className="mt-10">
-           
-          {auth ? (
-                        <button type="button" onClick={() => addToBasket(furniture)} className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Agregar al carrito</button>
 
-          ) : (
-            <button type="button" onClick={() => addToBasket(furniture)} className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Iniciar sesión</button>
-          )}
-          </form>
-        </div>
-  
-        <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
-          <div>
-            <h3 className="sr-only">Descripción</h3>
-  
-            <div className="space-y-6">
-              <p className="text-base text-gray-900">{furniture?.description}</p>
+              <form className="mt-10">
+                {auth ? (
+                  <button
+                    type="button"
+                    onClick={() => addToBasket(furniture)}
+                    className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Agregar al carrito
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    onClick={() => addToBasket(furniture)}
+                    className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Iniciar sesión
+                  </button>
+                )}
+              </form>
+            </div>
+
+            <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+              <div>
+                <h3 className="sr-only">Descripción</h3>
+
+                <div className="space-y-6">
+                  <p className="text-base text-gray-900">
+                    {furniture?.description}
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
-  
         </div>
-      </div>
+      )}
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default DetailsFurniture
+export default DetailsFurniture;
